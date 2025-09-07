d71a6571602d1fabb5699404ebaf54aeeba51563
Halkano Malise <kudezx@gmail.com>
2025-09-07 17:51:55 +0300

    init

---
+++ pos-system/src/test/java/com/kude/pos/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kude.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
