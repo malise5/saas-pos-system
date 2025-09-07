e1411548fe7d81df5c83cea85ac6e15a451e67e6
Halkano Malise <kudezx@gmail.com>
2025-09-08 00:25:00 +0300

    Implement JWT provider for token creation and email claim extraction

---
+++ pos-system/src/main/java/com/kude/pos/config/JwtProvider.java
@@ -0,0 +1,54 @@
+package com.kude.pos.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class JwtProvider {
+
+    private static final SecretKey key = Keys.hmacShaKeyFor(JwtConstant.JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String email, String authorities) {
+        return Jwts.builder()
+                .claim("email", email)
+                .claim("authorities", authorities)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + JwtConstant.JWT_EXPIRATION_MS))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getEmailFromToken(String jwtToken) {
+        if (jwtToken != null && jwtToken.startsWith("Bearer ")) {
+            jwtToken = jwtToken.substring(7);
+        }
+        return String.valueOf(Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload()
+                .get("email"));
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            token = token.startsWith("Bearer ") ? token.substring(7) : token;
+            Date expiration = Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload()
+                    .getExpiration();
+            return expiration.after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+
+}
